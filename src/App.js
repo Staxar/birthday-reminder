597e66f3e1824e16f56799dbd1f47d6b21063dec
Staxar <brzeszczot90@gmail.com>
2022-09-16 13:48:50 +0200

    Implement List people

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <div className="App"></div>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.lenght} birthdays today</h3>
+        <List people={people}></List>
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
